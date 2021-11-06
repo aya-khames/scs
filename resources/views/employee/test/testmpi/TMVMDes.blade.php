c088af2da8253ba8113b908409e64f429aae2802
aya-khames <ya443858@gmail.com>
2021-11-06 21:48:21 +0200

    all tables

--- resources/views/employee/test/testmpi/TMVMDes.blade.php
+++ resources/views/employee/test/testmpi/TMVMDes.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.employeepage')
 @section('content_1')
 
-    <div style="border-radius: 20px; border: rgba(15,70,108,0.66); box-shadow: 0 0 5px 5px gainsboro; position: fixed; margin-top: 80px; margin-left: 400px; z-index: 20; height: 780px; width: 1300px; background-color: rgba(240,248,248,0.57)">
+    <div style="border-radius: 20px; border: rgba(15,70,108,0.66); box-shadow: 0 0 5px 5px gainsboro; position: absolute; margin-top: 80px; margin-left: 400px; z-index: 20; height: 780px; width: 1300px; background-color: rgba(240,248,248,0.57)">
         <fieldset>
             <legend style="padding: 10px; color: #0b3756; font-family: 'Times New Roman'; font-size: 30px; font-weight: bold">Test_MPI & Visual_MPI</legend>
             <nav id="main-navbar" style="background-color: rgba(240,248,248,0.39); padding: unset" class="navbar navbar-expand-lg navbar-light bg-white">
@@ -15,27 +15,29 @@
                     <a>Print</a>
                 </div>
             </nav>
-            <form style="margin: 20px; box-shadow: 0 0 20px rgba(15,70,108,0.65); width: 1250px">
-                <div style="padding: 20px; border-radius: 5px; background-color: rgba(240,248,248,0.05)">
-                    <label class="lab" style="font-size: 20px; width: 140px">Report No.</label> <input class="text2" style="width: 400px; margin-right: 20px" type="text">
-                    <span class="sp">
-                        <a style="margin-left: 10px" href="#">Search</a>
+            <br>
+            <div style="margin: 5px; height: 600px; overflow-y: auto; width: 1280px">
+                <form style="margin: 20px; box-shadow: 0 0 20px rgba(15,70,108,0.65); width: 1190px">
+                    <div style="padding: 20px; border-radius: 5px; background-color: rgba(240,248,248,0.05)">
+                        <label class="lab" style="font-size: 20px; width: 140px">Report No.</label> <input class="text2" style="width: 400px; margin-right: 20px" type="text">
+                        <span class="sp">
+                        <a style="margin-left: 10px; cursor: pointer" onclick="showTable('table')">Search</a>
                     </span>
-                    <a class="sp" style="margin-left: 20px; text-decoration: none">Search Edit</a>
-                    <br>
-                    <label class="lab" style="font-size: 20px; width: 140px">Work Order:</label> <input readonly disabled id="work" class="text2" style="width: 400px; margin-right: 20px" type="text">
-                    <label class="lab" style="font-size: 20px; width: 70px; margin-left: 20px">Client:</label> <input readonly disabled id="client" class="text2" style="width: 400px" type="text">
-                    <br>
-                    <label class="lab" style="font-size: 20px; width: 140px">Proof load:</label> <input disabled id="pl" class="text2" style="width: 400px; margin-right: 20px" type="text">
-                    <label class="lab" style="font-size: 20px; width: 70px; margin-left: 20px">QTY:</label> <input disabled id="qty" class="text2" style="width: 400px" type="text">
-                    <br>
-                    <label class="lab" style="font-size: 17px; width: 140px">Safe working load:</label> <input disabled id="safe" class="text2" style="width: 400px; margin-right: 20px" type="text">
-                    <label class="lab" style="font-size: 20px; width: 70px; margin-left: 20px">ID No.</label> <input disabled id="pf" class="text2" style="width: 400px" type="number">
-                    <br>
-                    <label class="lab" style="font-size: 20px; width: 130px">Description:</label>
-                    <br>
-                    <textarea disabled id="disc" class="text2" style="width: 400px;margin-left: 145px; height: 150px; scroll-behavior: smooth; resize: none"></textarea>
-                    <span>
+                        <a class="sp" style="margin-left: 20px; text-decoration: none">Search Edit</a>
+                        <br>
+                        <label class="lab" style="font-size: 20px; width: 140px">Work Order:</label> <input readonly disabled id="work" class="text2" style="width: 400px; margin-right: 20px" type="text">
+                        <label class="lab" style="font-size: 20px; width: 70px; margin-left: 20px">Client:</label> <input readonly disabled id="client" class="text2" style="width: 400px" type="text">
+                        <br>
+                        <label class="lab" style="font-size: 20px; width: 140px">Proof load:</label> <input disabled id="pl" class="text2" style="width: 400px; margin-right: 20px" type="text">
+                        <label class="lab" style="font-size: 20px; width: 70px; margin-left: 20px">QTY:</label> <input disabled id="qty" class="text2" style="width: 400px" type="text">
+                        <br>
+                        <label class="lab" style="font-size: 17px; width: 140px">Safe working load:</label> <input disabled id="safe" class="text2" style="width: 400px; margin-right: 20px" type="text">
+                        <label class="lab" style="font-size: 20px; width: 70px; margin-left: 20px">ID No.</label> <input disabled id="pf" class="text2" style="width: 400px" type="number">
+                        <br>
+                        <label class="lab" style="font-size: 20px; width: 130px">Description:</label>
+                        <br>
+                        <textarea disabled id="disc" class="text2" style="width: 400px;margin-left: 145px; height: 150px; scroll-behavior: smooth; resize: none"></textarea>
+                        <span>
                         <div style="margin-left: 120px; display: inline-block">
                             <button disabled class="bttn">Print</button>
                             <button class="bttn">Delete</button>
@@ -43,8 +45,22 @@
                             <button class="bttn">Insert</button>
                         </div>
                     </span>
+                    </div>
+                </form>
+                <br>
+                <div style="margin: 20px; box-shadow: 0 0 20px rgba(15,70,108,0.65); width: 1190px; max-height: 400px; overflow-y: auto">
+                    <table id="table" style="display: none; width: 1190px">
+                        <tr style="color: white; background-color: #0b3756; cursor: default">
+                            <th>Company</th>
+                            <th>Contact</th>
+                            <th>Country</th>
+                            <th>Company</th>
+                            <th>Contact</th>
+                            <th>Country</th>
+                        </tr>
+                    </table>
                 </div>
-            </form>
+            </div>
         </fieldset>
     </div>
 @stop
--- resources/views/employee/wall/wallThickDes.blade.php
+++ resources/views/employee/wall/wallThickDes.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.employeepage')
 @section('content_1')
 
-    <div style="border-radius: 20px; border: rgba(15,70,108,0.66); box-shadow: 0 0 5px 5px gainsboro; position: fixed; margin-top: 80px; margin-left: 400px; z-index: 20; height: 780px; width: 1300px; background-color: rgba(240,248,248,0.57)">
+    <div style="border-radius: 20px; border: rgba(15,70,108,0.66); box-shadow: 0 0 5px 5px gainsboro; position: absolute; margin-top: 80px; margin-left: 400px; z-index: 20; height: 780px; width: 1330px; background-color: rgba(240,248,248,0.57)">
         <fieldset>
             <legend style="padding: 10px; color: #0b3756; font-family: 'Times New Roman'; font-size: 35px; font-weight: bold">Wall Thickness</legend>
             <nav id="main-navbar" style="background-color: rgba(240,248,248,0.39); padding: unset" class="navbar navbar-expand-lg navbar-light bg-white">
@@ -15,44 +15,60 @@
                     <a>Print</a>
                 </div>
             </nav>
-            <form style="margin: 20px; box-shadow: 0 0 20px rgba(15,70,108,0.65); width: 1250px">
-                <div style="padding: 20px; border-radius: 5px; background-color: rgba(240,248,248,0.05)">
-                    <label class="lab" style="font-size: 20px; width: 140px">Report No.</label> <input class="text2" style="width: 400px" type="text">
-                    <span class="sp">
-                        <a style="margin-left: 10px" href="#">Search</a>
+            <br>
+            <div style="margin: 5px; height: 600px; overflow-y: auto; width: 1320px">
+                <form style="margin: 20px; box-shadow: 0 0 20px rgba(15,70,108,0.65); width: 1250px">
+                    <div style="padding: 20px; border-radius: 5px; background-color: rgba(240,248,248,0.05)">
+                        <label class="lab" style="font-size: 20px; width: 140px">Report No.</label> <input class="text2" style="width: 400px" type="text">
+                        <span class="sp">
+                        <a style="margin-left: 10px; cursor: pointer" onclick="showTable('table')">Search</a>
                     </span>
-                    <a class="sp" style="margin-left: 20px; text-decoration: none">Search Edit</a>
-                    <br>
-                    <label class="lab" style="font-size: 20px; width: 140px">Client:</label>
-                    <input disabled id="client" class="text2" style="width: 400px" type="text">
-                    <label class="lab" style="font-size: 20px; width: 170px; margin-left: 20px">Work Order:</label>
-                    <input disabled id="work" class="text2" style="width: 400px" type="text">
-                    <br>
+                        <a class="sp" style="margin-left: 20px; text-decoration: none">Search Edit</a>
+                        <br>
+                        <label class="lab" style="font-size: 20px; width: 140px">Client:</label>
+                        <input disabled id="client" class="text2" style="width: 400px" type="text">
+                        <label class="lab" style="font-size: 20px; width: 170px; margin-left: 20px">Work Order:</label>
+                        <input disabled id="work" class="text2" style="width: 400px" type="text">
+                        <br>
 
-                    <br>
-                    <label class="lab" style="font-size: 20px; width: 140px">ITEM:</label>
-                    <input disabled id="item" class="text2" style="width: 400px" type="text">
-                    <label class="lab" style="font-size: 20px; width: 170px; margin-left: 20px">Original Thickness:</label>
-                    <input disabled id="ot" class="text2" style="width: 400px" type="text">
-                    <br>
+                        <br>
+                        <label class="lab" style="font-size: 20px; width: 140px">ITEM:</label>
+                        <input disabled id="item" class="text2" style="width: 400px" type="text">
+                        <label class="lab" style="font-size: 20px; width: 170px; margin-left: 20px">Original Thickness:</label>
+                        <input disabled id="ot" class="text2" style="width: 400px" type="text">
+                        <br>
 
-                    <br>
-                    <label class="lab" style="font-size: 20px; width: 140px">Min Thickness:</label>
-                    <input disabled id="mint" class="text2" style="width: 400px" type="text">
-                    <label class="lab" style="font-size: 20px; width: 170px; margin-left: 20px">Max Thickness:</label>
-                    <input disabled id="maxt" class="text2" style="width: 400px" type="text">
-                    <br>
+                        <br>
+                        <label class="lab" style="font-size: 20px; width: 140px">Min Thickness:</label>
+                        <input disabled id="mint" class="text2" style="width: 400px" type="text">
+                        <label class="lab" style="font-size: 20px; width: 170px; margin-left: 20px">Max Thickness:</label>
+                        <input disabled id="maxt" class="text2" style="width: 400px" type="text">
+                        <br>
 
-                    <label class="lab" style="font-size: 20px; width: 140px">Remark:</label>
-                    <input disabled id="remark" type="text" class="text2" style="width: 400px; height: 100px; resize: none">
-                    <div style="margin-left: 450px">
-                        <button disabled class="bttn">Print</button>
-                        <button class="bttn">Delete</button>
-                        <button class="bttn">Edit</button>
-                        <button class="bttn">Insert</button>
+                        <label class="lab" style="font-size: 20px; width: 140px">Remark:</label>
+                        <input disabled id="remark" type="text" class="text2" style="width: 400px; height: 100px; resize: none">
+                        <div style="margin-left: 450px">
+                            <button disabled class="bttn">Print</button>
+                            <button class="bttn">Delete</button>
+                            <button class="bttn">Edit</button>
+                            <button class="bttn">Insert</button>
+                        </div>
                     </div>
+                </form>
+                <br>
+                <div style="margin: 20px; box-shadow: 0 0 20px rgba(15,70,108,0.65); width: 1250px; max-height: 400px; overflow-y: auto">
+                    <table id="table" style="display: none; width: 1250px">
+                        <tr style="color: white; background-color: #0b3756; cursor: default">
+                            <th>Company</th>
+                            <th>Contact</th>
+                            <th>Country</th>
+                            <th>Company</th>
+                            <th>Contact</th>
+                            <th>Country</th>
+                        </tr>
+                    </table>
                 </div>
-            </form>
+            </div>
         </fieldset>
     </div>
 @stop
