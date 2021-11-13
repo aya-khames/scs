6dd77f546d29f2ea90a1a12f0bad5d9e87b55139
FarahMourad <51189763+FarahMourad@users.noreply.github.com>
2021-11-13 19:47:55 +0200

    same problem

--- app/Http/Controllers/SCS/CompressorController.php
+++ app/Http/Controllers/SCS/CompressorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\SCS;
 use App\Http\Controllers\Controller;
 use App\Models\Client;
 use App\Models\Compressor;
+use App\Models\Compressoritem;
 use Illuminate\Http\Request;
 
 class CompressorController extends Controller
@@ -45,7 +46,56 @@ class CompressorController extends Controller
                 $comp->save();
             }
         }
-
+        return redirect()->back();
+    }
+    ###################DESC###########################
+    public function searchCD(Request $request){
+        if ($request->searchType === "search"){
+            if ($request->quote === "empty"){
+                $c = Compressor::all();
+            } else{
+                $c = Compressor::where('CRET_NO', $request->quote)->get();
+            }
+        } else{
+            if ($request->quote === "empty"){
+                $c = Compressoritem::all();
+            } else{
+                $c = Compressoritem::where('CRET_NO', $request->quote)->get();
+            }
+        }
+        return response()->json($c);
+    }
+    public function insertCD(Request $request){
+        $t = Compressoritem::where('_id', $request->id)->first();
+        if ($t === null){
+            $t = new Compressoritem();
+            $t->Name_C = $request->client;
+            $t->ID_WO = $request->work;
+            $t->CRET_NO = $request->certNo;
+            $t->ID_NUM = $request->idNumber;
+            $t->QTY = $request->qty;
+            $t->Description = $request->description;
+            $t->Pressure = $request->press;
+            $t->save();
+        }
+        return redirect()->back();
+    }
+    public function editCD(Request $request){
+        $t = Compressoritem::where('_id', $request->id)->first();
+        if ( $t !== null){
+            $t->ID_NUM = $request->idNumber;
+            $t->QTY = $request->qty;
+            $t->Description = $request->description;
+            $t->Pressure = $request->press;
+            $t->save();
+        }
+        return redirect()->back();
+    }
+    public function deleteCD(Request $request){
+        $t = Compressoritem::where('_id', $request->id)->first();
+        if ($t !== null){
+            $t->delete();
+        }
         return redirect()->back();
     }
 
